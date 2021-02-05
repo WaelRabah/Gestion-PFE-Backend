590f4bd6e507b1c13fa37d9593733f5d59b97f9b
WaelRabah <Wael.Rabah@insat.u-carthage.tn>
2021-02-05 13:43:49 +0100

    final commit nodemailer

--- src/utils/emailSender.ts
+++ src/utils/emailSender.ts
@@ -1,15 +1,29 @@
 import * as nodemailer from 'nodemailer';
+import  {google , gmail_v1} from 'googleapis'
+import * as htmlToText from 'html-to-text'
+const OAuth2 = google.auth.OAuth2;
 
 export const send = async (to, from, subject, html) => {
-  // let testAccount = await nodemailer.createTestAccount();
+  const myOAuth2Client = new OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    )
+
+    myOAuth2Client.setCredentials({
+      refresh_token: process.env.REFRESH_TOKEN
+      });
+      const myAccessToken = await myOAuth2Client.getAccessToken()
+
+      
   let transporter = nodemailer.createTransport({
+    
     host: "smtp.gmail.com",
     port: 465,
     secure: true, 
     auth: {
       type: 'OAuth2',
       user: process.env.FROM_EMAIL,
-      accessToken: process.env.ACCESS_TOKEN
+      accessToken: myAccessToken.token,
   },
   });
   transporter.verify(function(error, success) {
@@ -19,12 +33,14 @@ export const send = async (to, from, subject, html) => {
       console.log("Server is ready to take our messages");
     }
   });
+  const text = htmlToText.htmlToText(html)
+  
   let info = await transporter.sendMail(
     {
       from,
       to  ,
       subject,
-      html,
+      text,
     },
     (err, info) => {
       if (err) {
